c5ff07c362988967d7a2b26a8bfcf1799f4cd842
Justyna Puz <puzjustyna@gmail.com>
2024-03-28 20:17:16 +0100

    Added csv util and kigdom getCard

--- src/main/java/cardgame/Kingdom.java
+++ src/main/java/cardgame/Kingdom.java
@@ -11,6 +11,39 @@ public class Kingdom {
         for(Attribute attribute : Attribute.values()) {
             int randomValue = rand.nextInt(40,91);
             attributes.put(attribute,randomValue);
+        }
+    }
+
+    public  void getCard(int Id, Boolean isAccepted) {
+
+       Card card =  CardLoadUtil.cards.get(Id);
+       if(isAccepted) {
+           changeAttributes(card.getAttributesAccepted());
+       }else {
+           changeAttributes(card.getAttributesNotAccepted());
+       }
+    }
+
+    private void changeAttributes(EnumMap<Attribute, Integer> newAttributes) {
+        Random random = new Random();
+        int value;
+        for(Attribute attribute : Attribute.values()) {
+            if(newAttributes.get(attribute) != 0) {
+                int min = newAttributes.get(attribute) - 6;
+                int max = newAttributes.get(attribute) + 5;
+                int newAttributeValue = random.nextInt(max - min) + min;
+                 value = attributes.get(attribute) + newAttributeValue;
+            }else {
+                value= 0;
+                attributes.put(attribute, value);
+                continue;
+            }
+            if(value < 0) {
+                value =0;
+            }else if(value > 100) {
+                value = 100;
+            }
+            attributes.put(attribute, value);
 
         }
     }
--- src/main/java/cardgame/Main.java
+++ src/main/java/cardgame/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Kingdom kingdom = new Kingdom();
+        CardLoadUtil.loadCard();
+        kingdom.getCard(1,true);
 
     }
 }
